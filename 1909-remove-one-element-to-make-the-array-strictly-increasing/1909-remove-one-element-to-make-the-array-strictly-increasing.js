3c91ff26f5f5f58e9361d1c1333b8472b89692ee
Aayush Kumar Sinha <223195287+aayushks38@users.noreply.github.com>
2026-07-31 15:12:16 +0530

    Time: 0 ms (100%), Space: 54.8 MB (27.45%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canBeIncreasing = function(nums) {
+    let count = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            if (count === 1) {
+                return false;
+            }
+
+            count++;
+
+            if (i > 1 && nums[i] <= nums[i - 2]) {
+                nums[i] = nums[i - 1];
+            }
+        }
+    }
+
+    return true;
+};
